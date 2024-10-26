5e01e081f12cf21b220a7540244f4d8bc149ad3b
ValeriaCastro04 <vpcastro344@gmail.com>
2024-10-25 18:40:22 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,9 +1,32 @@
-import { createConnection } from 'mysql2'
-import second from 'mysql2/promise'
+const express = require('express');
+const mysql = require('mysql');
 
-createConnection({
+const app = express();
+const connection = mysql.createConnection({
+    host: 'localhost',   
     user: 'root',
     password: 'root',
-    host: 'localhost',
-    port: 3307,
-})
+    database: 'marketplace'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Conectado a la base de datos');
+});
+
+app.listen(3007, () => console.log('Servidor corriendo en el puerto 3007'));
+
+app.get('/api/datos', (req, res) => {
+    connection.query('SELECT * FROM tabla', (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+fetch('http://localhost:3007/api/datos')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data); // Aquí puedes trabajar con los datos recibidos
+    })
+    .catch(error => console.error('Error:', error));
+
