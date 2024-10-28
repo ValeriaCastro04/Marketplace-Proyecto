c4424bc66fe559abc350040e9a4c51b400e838fc
ValeriaCastro04 <vpcastro344@gmail.com>
2024-10-28 14:13:10 -0600

    update carrito de compras

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
-const btnCart = document.querySelector('.container-icon')
+const btnCart = document.querySelector('.container-cart-icon')
 
 const containeCartProducts = document.querySelector(".container-cart-products")
 
 btnCart.addEventListener("click", () => {
     containeCartProducts.classList.toggle('hidden-cart')
 })
+
+//############################################################33
+const cartInfo = document.querySelector('.cart-product')
